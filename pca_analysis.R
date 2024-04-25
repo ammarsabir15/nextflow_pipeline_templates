054f9365245b8a22d1e4f6411bafa83a02a74aa7
Ammar Sabir Cheema <ammarsabir15@gmail.com>
2024-04-25 20:36:08 -0300

    Update pca_analysis.R

--- pca_analysis.R
+++ pca_analysis.R
@@ -7,10 +7,13 @@ input_data <- read.csv(commandArgs(TRUE)[1],sep = "\t",row.names = 1)
 # Perform PCA
 pca_result <- prcomp(input_data)
 
+pcaData <- as.data.frame(pca_result$x[, 1:2]) # extract first two columns and convert to data frame
+pcaData <- cbind(pcaData, iris$Species) # bind by columns
+colnames(pcaData) <- c("PC1", "PC2", "Species")
 
-pca_plot <- ggplot(data = as.data.frame(pca_result$x), aes(x = PC1, y = PC2)) +
+pca_plot <- ggplot(data = pcaData, aes(x = PC1, y = PC2,color = Species, shape = Species)) +
                     geom_point() +
-                                labs(x = "PC1", y = "PC2", title = "PCA Plot")
+                              labs(x = "PC1", y = "PC2", title = "PCA Plot")
 
 
 #output_file <- commandArgs(TRUE)[2]
